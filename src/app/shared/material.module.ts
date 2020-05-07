6ce965767ea4e8b6549cf71a40ceb61e18cadbf6
GR <Gabriel.Raducu@externals.adidas.com>
2020-05-07 23:01:29 +0200

    Enrich material module

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -3,12 +3,18 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
 
 const importExport = [
   MatButtonModule,
   MatFormFieldModule,
   MatInputModule,
   MatToolbarModule,
+  MatIconModule,
+  MatTableModule,
+  MatSortModule
 ];
 
 @NgModule({
